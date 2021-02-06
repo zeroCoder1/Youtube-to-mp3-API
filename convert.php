faaa1505b7579646ab343b7b5401e0bbfb9d64c3
Techno Cyrex <65451773+Technocyrex@users.noreply.github.com>
2021-02-06 20:34:40 +0530

    Update convert.php

--- convert.php
+++ convert.php
@@ -12,7 +12,7 @@ header("Content-Type: application/json");
 if(isset($_GET["youtubelink"]) && !empty($_GET["youtubelink"]))
 {
 	$youtubelink = $_GET["youtubelink"];
-        $yt->setBinPath('/app/.heroku/youtube-dl');
+        $dl->setBinPath('/app/.heroku/youtube-dl');
 
 	$success = preg_match('#(?<=v=)[a-zA-Z0-9-]+(?=&)|(?<=v\/)[^&\n]+|(?<=v=)[^&\n]+|(?<=youtu.be/)[^&\n]+#', $youtubelink, $matches);
 
